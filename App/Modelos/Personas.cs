f4f908e255fc94442e2dab6ee885c796226b5c48
Lguerr <leonidas_jose_guerrero@hotmail.com>
2015-03-02 18:57:52 -0500

    Hoy

---
+++ App/Modelos/Personas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelos
+{
+    public class Personas
+    {
+       private string id;
+
+public string Id
+{
+  get { return id; }
+  set { id = value; }
+}
+       private string nombre;
+
+public string Nombre
+{
+  get { return nombre; }
+  set { nombre = value; }
+}
+       private string direccion;
+
+public string Direccion
+{
+  get { return direccion; }
+  set { direccion = value; }
+}
+      private  string telefono;
+
+public string Telefono
+{
+  get { return telefono; }
+  set { telefono = value; }
+}
+     private   string email;
+
+public string Email
+{
+  get { return email; }
+  set { email = value; }
+}
+
+    }
+    
+}
